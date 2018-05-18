fd984bf8b58337f006862319527062962b983aa8
florian <florian@meilleurescpi.com>
2018-05-18 15:43:20 +0200

    debut dictionnaire

---
+++ dictionnaire_supprimer.py
@@ -0,0 +1,12 @@
+#premier methode avec del
+
+#placard = {"chemise":3, "pantalon":6, "tee shirt":7}
+#del placard["chemise"]
+
+#deuxieme avec la methode pop qui renvoi la valeur supprimé
+placard = {"chemise":3, "pantalon":6, "tee shirt":7}
+placard.pop("chemise")
+
+
+ok = {"florian":1}
+print(ok.pop("florian"))
